3df74594b3d04a36939088d9c3210d9011ab403c
JesusWhite97 <jesus120190240.8@gmail.com>
2020-03-26 21:11:32 -0700

    master

--- IngexisLabSoft/BD/Query Tablas.sql
+++ IngexisLabSoft/BD/Query Tablas.sql
@@ -1,4 +1,4 @@
-use databaseingexis;
+--use databaseingexis;
 --======================================--
 -- create table Log_usuarios(
 --     id_usuario      int             primary key     not null    AUTO_INCREMENT,
@@ -7,24 +7,24 @@ use databaseingexis;
 --     img_log         VARCHAR(100)
 -- );
 --======================================--
-create table usuarios(
-    id_usuario      int             not null,
-    nombre1         varchar(20),
-    nombre2         varchar(20),
-    Primer_ape      varchar(30),
-    Segund_ape      varchar(30),
-    --informacion---------------
-    Num_contacto    varchar(30),
-    puesto          VARCHAR(10),
-    Curp            varchar(18),
-    rfc             varchar(13),
-    --direccion-----------------
-    calleP          varchar(20),
-    Entrecalles     varchar(50),
-    numero          varchar(10),
-    colonia         varchar(20),
-    cod_postal      varchar(10),
-    --ralaciones ETC------------
-    FOREIGN key(id_usuario) REFERENCES Log_usuarios(id_usuario)
-);
+-- create table usuarios(
+--     id_usuario      int             not null,
+--     nombre1         varchar(20),
+--     nombre2         varchar(20),
+--     Primer_ape      varchar(30),
+--     Segund_ape      varchar(30),
+--     --informacion---------------
+--     Num_contacto    varchar(30),
+--     puesto          VARCHAR(10),
+--     Curp            varchar(18),
+--     rfc             varchar(13),
+--     --direccion-----------------
+--     calleP          varchar(20),
+--     Entrecalles     varchar(50),
+--     numero          varchar(10),
+--     colonia         varchar(20),
+--     cod_postal      varchar(10),
+--     --ralaciones ETC------------
+--     FOREIGN key(id_usuario) REFERENCES Log_usuarios(id_usuario)
+-- );
 --======================================--
